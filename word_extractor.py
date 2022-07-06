7f8ee6f1d1e7cacabccd017547e810170538cedc
av <allexeyv@icloud.com>
2022-07-06 06:45:48 +0100

    pre-commit

--- word_extractor.py
+++ word_extractor.py
@@ -1,23 +1,32 @@
 import glob
-import yake
+
 import nltk
 import pandas as pd
+import yake
 from fire import Fire
 
 
 class WordExtractor:
     """Class provides extracting key words (interesting words) from a list of text files using YAKE! algorithm.
-    
+
     Attributes:
         lang (str, optional): A parameter to determin text language. Defaults to 'en' (English).
         num_of_keywords (int, optional): How many key words should be in the result from a sentence. Defaults to 3.
-        keyword_limit_in_final_table (int, optional): A filter parameter for the result table. Cuts unpopular words. Defaults to 3.
-        keyword_list (dict): Stores all extracted key words from text. Data for the result table. {'keyword': {'count': 0, 'source': [], 'sentence': []}}
-        keyword_list_filtered (dict): Stores only popular key words, the limit is in the than keyword_limit_in_final_table parameter.
+        keyword_limit_in_final_table (int, optional): A filter parameter for the result table. Cuts unpopular words.
+            Defaults to 3.
+        keyword_list (dict): Stores all extracted key words from text. Data for the result table.
+            The format is {'keyword': {'count': 0, 'source': [], 'sentence': []}}
+        keyword_list_filtered (dict): Stores only popular key words, the limit is in the than
+            keyword_limit_in_final_table parameter.
         blacklist (list): Unwanted key words.
     """
 
-    def __init__(self, lang: str='en', num_of_keywords: int=3, keyword_limit_in_final_table: int=3):
+    def __init__(
+        self,
+        lang: str = 'en',
+        num_of_keywords: int = 3,
+        keyword_limit_in_final_table: int = 3,
+    ):
         self.max_ngram_size = 1
         self.deduplication_threshold = 0.9
         self.custom_kw_extractor = yake.KeywordExtractor(
@@ -29,10 +38,13 @@ class WordExtractor:
         self.keyword_limit_in_result = keyword_limit_in_final_table
         self.keyword_list = {}
         self.keyword_list_filtered = {}
-        self.blacklist = ['year', 'years', 'day', 'time', 'make', 'made', 'easy', 'hard', 'stop', 'good', 'looked',
-                        'long', 'making', 'left', 'stand', 'pass', 'true', 'ago', 'longer', 'times', 'turned', 'greater', 
-                        'and', 'thing', 'told', 'thought', 'changed', 'words', 'fact'
+        # fmt: off
+        self.blacklist = [
+            'year', 'years', 'day', 'time', 'make', 'made', 'easy', 'hard', 'stop', 'good', 'looked',
+            'long', 'making', 'left', 'stand', 'pass', 'true', 'ago', 'longer', 'times', 'turned', 'greater',
+            'and', 'thing', 'told', 'thought', 'changed', 'words', 'fact',
         ]
+        # fmt: on
 
     def _clean_text(self, text: str) -> str:
         """Prepares text for the future operations.
@@ -105,8 +117,7 @@ class WordExtractor:
             }
 
     def _filter_rows(self) -> None:
-        """Filter rows to remove not popular key words
-        """
+        """Filter rows to remove not popular key words"""
         for k, v in self.keyword_list.items():
             if k in self.blacklist:
                 continue
@@ -114,7 +125,8 @@ class WordExtractor:
                 self.keyword_list_filtered[k] = v
 
     def process_file_list(self, file_list: list) -> None:
-        """Extract key words from list of text files and stores them into self.keyword_list and self.keyword_list_filtered
+        """Extract key words from list of text files and stores them into self.keyword_list
+        and self.keyword_list_filtered
         Args:
             file_list (list): list of text files
         """
@@ -127,17 +139,21 @@ class WordExtractor:
 
 
 def main(folder, lang='en', num_of_keywords=3, keyword_limit_in_final_table=3):
-    """Launches the program.
-    """
+    """Launches the program."""
     nltk.download('punkt')
     file_list = glob.glob(f'{folder}/*txt')
-    WE = WordExtractor(lang=lang, num_of_keywords=num_of_keywords, keyword_limit_in_final_table=keyword_limit_in_final_table)
+    WE = WordExtractor(
+        lang=lang,
+        num_of_keywords=num_of_keywords,
+        keyword_limit_in_final_table=keyword_limit_in_final_table,
+    )
     WE.process_file_list(file_list)
     df = pd.DataFrame(WE.keyword_list_filtered).transpose()
     df = df.sort_values(by=['count'], ascending=False)
     df = df.reset_index().rename({'index': 'word'}, axis=1)
     df.to_csv('key_words_extracted.csv', sep='\t', encoding='utf-8')
-    print('The result csv is ready.')
+    print('The result csv is ready.')  # noqa: T201
+
 
 if __name__ == '__main__':
     Fire(main)
